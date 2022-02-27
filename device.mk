036800660bec4540a85793c4e40428f65e1fdca9
Akash Srivastava <akashniki@gmail.com>
2022-02-27 17:23:19 +0530

    cmi: Switch to QTI AV component

--- device.mk
+++ device.mk
@@ -228,8 +228,7 @@ PRODUCT_PACKAGES += \
 
 # Media
 PRODUCT_PACKAGES += \
-    libavservices_minijail \
-    libavservices_minijail.vendor
+    libavservices_minijail
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media/media_codecs_c2.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_c2.xml \
@@ -319,6 +318,7 @@ PRODUCT_PACKAGES += \
 
 # QTI
 TARGET_COMMON_QTI_COMPONENTS := \
+    av \
     perf
 
 # RenderScript
